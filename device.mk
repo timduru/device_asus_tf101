dc950be050a03273f80673678516f415ff07622c
Donald Munn <donald.munn@gmail.com>
2011-11-27 01:03:58 +0000

    Remove mtp from persist.sys.usb.config which was preventing adb from working properly.
    Change-Id: I74dfc96f1257d9b784b7c83e6a12058e1a0744f3

--- device.mk
+++ device.mk
@@ -123,7 +123,7 @@ PRODUCT_PROPERTY_OVERRIDES := \
 
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp
+    persist.sys.usb.config=adb
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
