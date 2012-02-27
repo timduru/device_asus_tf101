f8a39a8fdab9785bc3d239e7b8052fe3750196ee
Donald Munn <donald.munn@gmail.com>
2012-02-27 17:14:58 +0000

    Add custom releasetools to support blobs and omit recovery from ota image
    Change-Id: I4b5fba66e72940a4697540857856a6109f51cfd5

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,5 +4,10 @@ include $(CLEAR_VARS)
 
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
+subdir_makefiles:= \
+    $(LOCAL_PATH)/blobpack/Android.mk \
+
+include $(subdir_makefiles)
+
 # include the non-open-source counterpart to this file
 -include vendor/asus/tf101/AndroidBoardVendor.mk
